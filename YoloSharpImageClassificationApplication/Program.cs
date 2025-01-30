f56fa7bbaa78778dcd3ca0c061f2679c3bb12dc4
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-01-31 12:49:56 +1300

    Back integrating Yolo Object detection

---
+++ YoloSharpImageClassificationApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YoloSharpImageClassificationApplication
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
