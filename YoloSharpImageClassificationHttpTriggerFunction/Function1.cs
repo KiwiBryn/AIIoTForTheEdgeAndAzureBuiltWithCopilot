1d893d68dcc66345e16430ae9e3fe72085aa9912
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-01-31 21:40:50 +1300

    Working can process a single image

--- YoloSharpImageClassificationHttpTriggerFunction/Function1.cs
+++ YoloSharpImageClassificationHttpTriggerFunction/Function1.cs
@@ -1,5 +1,6 @@
 // please write an  httpTrigger azure function that uses YoloSharp and a Yolo image classification onnx model
 // Image classification not object detection
+// The image is in the form data
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -18,7 +19,6 @@ namespace YoloSharpImageClassificationHttpTriggerFunction
       public Function1(ILogger<Function1> logger)
       {
          _logger = logger;
-         ///_yoloModel = new YoloModel("path_to_your_onnx_model.onnx");
          _yoloModel = new YoloPredictor("yolov8s-cls.onnx");
       }
 
@@ -27,14 +27,22 @@ namespace YoloSharpImageClassificationHttpTriggerFunction
       {
          _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-         if (!req.ContentType.StartsWith("image/"))
+         if (!req.ContentType.StartsWith("multipart/form-data"))
          {
-            return new BadRequestObjectResult("Invalid content type. Please upload an image.");
+            return new BadRequestObjectResult("Invalid content type. Please upload an image in form data.");
+         }
+
+         var form = await req.ReadFormAsync();
+         var file = form.Files["image"];
+
+         if (file == null || file.Length == 0)
+         {
+            return new BadRequestObjectResult("No image uploaded.");
          }
 
          using (var memoryStream = new MemoryStream())
          {
-            await req.Body.CopyToAsync(memoryStream);
+            await file.CopyToAsync(memoryStream);
             var imageBytes = memoryStream.ToArray();
 
             var results = _yoloModel.Classify(imageBytes);
