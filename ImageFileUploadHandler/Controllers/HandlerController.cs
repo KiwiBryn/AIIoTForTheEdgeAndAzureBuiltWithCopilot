9f3391f5ec853c92672fbb698d8ddc94017a1961
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-02-20 18:36:18 +1300

    Added start of UploadFileHandler

---
+++ ImageFileUploadHandler/Controllers/HandlerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImageFileUploadHandler.Controllers
+{
+    public class HandlerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
