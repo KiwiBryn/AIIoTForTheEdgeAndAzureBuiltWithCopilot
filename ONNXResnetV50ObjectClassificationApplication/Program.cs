ef0bb9dba1ad3fb68ccf8e8c588261bfb4ca7e83
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-02-20 14:32:08 +1300

    First working version of ONNX ResnetV5-27 application and HTTP Trigger function

--- ONNXResnetV50ObjectClassificationApplication/Program.cs
+++ ONNXResnetV50ObjectClassificationApplication/Program.cs
@@ -1,7 +1,7 @@
 ﻿// please write a C# console application that uses Onnx to run a ResNet50 object detection model on an image loaded from disk
 // Modify to use imagesharp
 // Resize image to 224x224 and crop
-// Load image laebls from a file
+// Load image labels from a file
 using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
 //using System.Drawing;
@@ -17,17 +17,18 @@ namespace ONNXResnetV5ObjectClassificationApplication
       {
          string modelPath = "..\\..\\..\\..\\Models\\resnet50-v2-7.onnx"; // Path to the ONNX model
          string imagePath = "pizza.jpg"; // Path to the input image
+         string labelsPath = "labels.txt"; // Path to the labels file
 
          // Load the image
          using var image = Image.Load<Rgb24>(imagePath);
          image.Mutate(x => x.Resize(new ResizeOptions
          {
             Size = new Size(224, 224),
-            //Mode = ResizeMode.Crop
-            Mode = ResizeMode.BoxPad
+            Mode = ResizeMode.Crop
+            //Mode = ResizeMode.BoxPad
          }));
 
-         image.Save("dog-resized.jpeg");
+         image.Save("..\\..\\..\\pizza-resized.jpeg");
 
          var inputTensor = ImageToTensor(image);
 
@@ -37,18 +38,25 @@ namespace ONNXResnetV5ObjectClassificationApplication
          // Create input tensor
          var inputMeta = session.InputMetadata;
          var inputs = new List<NamedOnnxValue>
-                     {
-                         NamedOnnxValue.CreateFromTensor("data", inputTensor)
-                     };
+                        {
+                            NamedOnnxValue.CreateFromTensor("data", inputTensor)
+                        };
 
          // Run inference
          using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
 
          // Process the results
          var output = results.First().AsEnumerable<float>().ToArray();
-         var predictedLabel = output.ToList().IndexOf(output.Max());
+         var predictedLabelIndex = output.ToList().IndexOf(output.Max());
+
+         // Load labels
+         var labels = File.ReadAllLines(labelsPath);
+         var predictedLabel = labels[predictedLabelIndex];
 
          Console.WriteLine($"Predicted Label: {predictedLabel}");
+
+         Console.WriteLine("Press ENTER to exit");
+         Console.ReadLine();
       }
 
       private static DenseTensor<float> ImageToTensor(Image<Rgb24> image)
