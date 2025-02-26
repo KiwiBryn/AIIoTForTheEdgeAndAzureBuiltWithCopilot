811aae0f21379606d6b339b15bd6d7858cac36b7
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-02-26 13:14:56 +1300

    Fixed up shape of tensor

--- ONNXFasterRCNNObjectDetectionApplication/Program.cs
+++ ONNXFasterRCNNObjectDetectionApplication/Program.cs
@@ -1,6 +1,8 @@
 ﻿// please write a C# console application that uses Onnx to run a ONNX Faster-RCNN object detection model on an image loaded from disk
 //    Added System.Drawing.Common Microsoft.ML.OnnxRuntime NuGet package
 // Fixed up paths to the model and image.
+// please fix "Invalid rank for input: image Got: 4 Expected: 3 Please fix either the inputs/outputs or the model"
+// change dimensions of declaration tensor rather than reshaping it
 
 using System.Drawing;
 using System.Drawing.Imaging;
@@ -23,9 +25,9 @@ namespace ONNXFasterRCNNObjectDetectionApplication
 
          var inputTensor = ExtractTensorFromImage(image);
          var inputs = new List<NamedOnnxValue>
-               {
-                   NamedOnnxValue.CreateFromTensor("image", inputTensor)
-               };
+                  {
+                      NamedOnnxValue.CreateFromTensor("image", inputTensor)
+                  };
 
          using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = session.Run(inputs);
          var output = results.First().AsEnumerable<float>().ToArray();
@@ -41,16 +43,16 @@ namespace ONNXFasterRCNNObjectDetectionApplication
       {
          int width = image.Width;
          int height = image.Height;
-         var tensor = new DenseTensor<float>(new[] { 1, 3, height, width });
+         var tensor = new DenseTensor<float>(new[] { 3, height, width });
 
          for (int y = 0; y < height; y++)
          {
             for (int x = 0; x < width; x++)
             {
                Color pixel = image.GetPixel(x, y);
-               tensor[0, 0, y, x] = pixel.R / 255.0f;
-               tensor[0, 1, y, x] = pixel.G / 255.0f;
-               tensor[0, 2, y, x] = pixel.B / 255.0f;
+               tensor[0, y, x] = pixel.R / 255.0f;
+               tensor[1, y, x] = pixel.G / 255.0f;
+               tensor[2, y, x] = pixel.B / 255.0f;
             }
          }
 
