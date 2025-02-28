ec80b860b40815c0f49ff7d3362142da2ff3be2c
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-02-28 22:02:19 +1300

    Manually modified the code to load onnx file, set model type and not use GPU

--- YoloDotnetObjectDetectionHttpTriggerFunction/Function1.cs
+++ YoloDotnetObjectDetectionHttpTriggerFunction/Function1.cs
@@ -1,21 +1,33 @@
 // Modify the code so it uses YoloDotNet to detect objects an image in the POST form data
 // Modify the code to use YoloDotNet v2.2 from https://github.com/NickSwardh/YoloDotNet
+// Modify the code to use YoloDotNet
+// Modify the code t use SkiaSharp to load the image
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using YoloDotNet;
+using YoloDotNet.Enums;
+
 
 namespace YoloDotnetObjectDetectionHttpTriggerFunction
 {
    public class Function1
    {
       private readonly ILogger<Function1> _logger;
-      private readonly YoloWrapper _yolo; // This is from Alturos.Yolo
+      //private readonly YoloWrapper _yolo; // This is from Alturos.Yolo
+      private readonly Yolo _yolo; // This is from YoloDotNet
 
       public Function1(ILogger<Function1> logger)
       {
          _logger = logger;
-         _yolo = new YoloWrapper("yolov3.cfg", "yolov3.weights", "coco.names"); // This is from Alturos.Yolo
+         //_yolo = new YoloWrapper("yolov3.cfg", "yolov3.weights", "coco.names"); // This is from Alturos.Yolo
+         _yolo = new Yolo(new YoloDotNet.Models.YoloOptions()
+         {
+            Cuda = false,
+            OnnxModel = "yolov8s.onnx",
+            ModelType = ModelType.ObjectDetection,
+         });
       }
 
       [Function("ObjectDetectionFunction")]
