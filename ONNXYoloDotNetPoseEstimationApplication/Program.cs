29cbaa047bcbd31f99248ff2100ab3af5a7d2595
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-04-03 15:05:21 +1300

    First cut stubbed out YoloDotNet pose estimation

---
+++ ONNXYoloDotNetPoseEstimationApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ONNXYoloDotNetPoseEstimationApplication
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
