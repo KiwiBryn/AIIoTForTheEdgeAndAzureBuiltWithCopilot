ab31eaa08e30ca4eb42e92a69855d7b642f1bffc
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-04-07 14:45:06 +1200

    Stubbed out implementation

---
+++ YoloDotNetPoseEstimationApplicationCopilot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace YoloDotNetPoseEstimationApplicationCopilot
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
