7a84bd5ea92cccff0c137cf81b49a2a7c556a30d
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-04-22 15:02:54 +1200

    Updated to SixLabors but ImageLoad fail until position reset

--- ResnetV50ObjectClassificationHttpTriggerCoPilot/Function1.cs
+++ ResnetV50ObjectClassificationHttpTriggerCoPilot/Function1.cs
@@ -1,7 +1,6 @@
 using System;
 using System.IO;
 using System.Linq;
-using System.Drawing;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +11,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.ML.OnnxRuntime;
 using Microsoft.ML.OnnxRuntime.Tensors;
 //using Newtonsoft.Json;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 using Microsoft.Azure.Functions.Worker;
 
 public static class Function1
@@ -19,12 +21,11 @@ public static class Function1
    private static readonly ILogger logger;
    private static readonly InferenceSession session = new InferenceSession("resnet50-v2-7.onnx");
 
-   // Static constructor to initialize logger
    static Function1()
    {
       var loggerFactory = LoggerFactory.Create(builder =>
-      { 
-         builder.AddConsole(); 
+      {
+         builder.AddConsole();
       });
       logger = loggerFactory.CreateLogger("Function1Logger");
    }
@@ -39,7 +40,10 @@ public static class Function1
       {
          using var ms = new MemoryStream();
          await req.Body.CopyToAsync(ms);
-         using var image = Image.FromStream(ms);
+
+         ms.Seek(0, SeekOrigin.Begin);
+
+         using var image = Image.Load<Rgb24>(ms);
 
          var inputTensor = PreprocessImage(image);
 
@@ -47,7 +51,7 @@ public static class Function1
          var outputName = session.OutputMetadata.Keys.First();
          var inputList = new List<NamedOnnxValue> 
          { 
-            NamedOnnxValue.CreateFromTensor(inputName, inputTensor)
+            NamedOnnxValue.CreateFromTensor(inputName, inputTensor) 
          };
 
          var result = await Task.Run(() => session.Run(inputList));
@@ -79,9 +83,9 @@ public static class Function1
       }
    }
 
-   private static Tensor<float> PreprocessImage(Image image)
+   private static Tensor<float> PreprocessImage(Image<Rgb24> image)
    {
-      var resized = new Bitmap(image, new Size(224, 224));
+      image.Mutate(ctx => ctx.Resize(224, 224));
       var tensorData = new float[1 * 3 * 224 * 224];
 
       float[] mean = { 0.485f, 0.456f, 0.406f };
@@ -91,7 +95,7 @@ public static class Function1
       {
          for (int x = 0; x < 224; x++)
          {
-            var pixel = resized.GetPixel(x, y);
+            var pixel = image[x, y];
 
             tensorData[(0 * 3 * 224 * 224) + (0 * 224 * 224) + (y * 224) + x] = (pixel.R / 255.0f - mean[0]) / std[0];
             tensorData[(0 * 3 * 224 * 224) + (1 * 224 * 224) + (y * 224) + x] = (pixel.G / 255.0f - mean[1]) / std[1];
