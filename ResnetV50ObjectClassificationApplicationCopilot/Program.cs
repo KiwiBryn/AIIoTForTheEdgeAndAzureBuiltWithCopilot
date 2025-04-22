f415596946e7e95877a42fe90d8d962be64f5efa
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-04-22 12:57:47 +1200

    First cut of application stubbed out

---
+++ ResnetV50ObjectClassificationApplicationCopilot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ResnetV50ObjectClassificationApplicationCopilot
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
