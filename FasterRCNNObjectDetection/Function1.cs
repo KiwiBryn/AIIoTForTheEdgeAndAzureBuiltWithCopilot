4000502d0efa663f8403383e5016b7010cbe71b0
Bryn Lewis <bryn.lewis@devMobile.co.nz>
2025-05-28 22:31:10 +1200

    Added ML.NET onnx NuGet and loaded model + labels file. Went pop for consumption and  Flex consumption with files not found.

--- FasterRCNNObjectDetection/Function1.cs
+++ FasterRCNNObjectDetection/Function1.cs
@@ -2,16 +2,21 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.ML.OnnxRuntime;
 
 namespace FasterRCNNObjectDetection;
 
 public class Function1
 {
    private readonly ILogger<Function1> _logger;
+   private readonly List<string> _labels;
+   private readonly InferenceSession _session;
 
    public Function1(ILogger<Function1> logger)
    {
       _logger = logger;
+      _labels = File.ReadAllLines("labels.txt").ToList();
+      _session = new InferenceSession("FasterRCNN-10.onnx");
    }
 
    [Function("ObjectDetectionFunction")]
